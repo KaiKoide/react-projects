c64c2681fde21a4c19ea23daa2c11bb294dc51e4
Kai <kkai.chien0813@gmail.com>
2024-02-19 19:19:57 -0800

    feat: render  cards

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,53 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ pageData, setPageData }) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const handleClick = (data) => {
+    const newPageData = pageData.filter((infoData) => {
+      return infoData !== data;
+    });
+
+    setPageData(newPageData);
+  };
+
+  const handleChange = () => {
+    setIsExpand(!isExpand);
+  };
+
+  const mappedData = pageData.map((data) => {
+    return (
+      <div key={data.id} className="bg-white relative m-6 flex flex-col">
+        <img
+          src={data.image}
+          alt={data.name}
+          className="h-80 object-cover w-full"
+        />
+        <span className="absolute bg-green-500 text-white p-2 right-0 top-0">
+          ${data.price}
+        </span>
+        <p className="p-3 text-center font-bold text-xl">{data.name}</p>
+        <p className="p-3">
+          {isExpand ? `${data.info} ` : `${data.info.substring(0, 300)}... `}
+          <button onClick={handleChange} className="text-green-400">
+            {isExpand ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
+        <button
+          className="btn btn-outline btn-primary m-5"
+          onClick={() => handleClick(data)}
+        >
+          Not Interested
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="grid grid-cols-3">{mappedData}</div>
+    </>
+  );
 };
 
 export default Tour;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Tour from './Tour';
+
+const dataFetch = async () => {
+  const res = await fetch('https://course-api.com/react-tours-project');
+  const data = await res.json();
+  return data;
+};
+
 const Tours = () => {
-  return <h2>tours component</h2>;
+  const [pageData, setPageData] = useState([]);
+
+  useEffect(() => {
+    const getPageData = async () => {
+      const data = await dataFetch();
+      setPageData(data);
+    };
+    getPageData();
+  }, []);
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      {pageData.length > 0 ? (
+        <div className="flex flex-col items-center">
+          <h2 className="items-center pt-20 border-b-8 inline-block border-green-400 pb-5">
+            Our Tours
+          </h2>
+          <Tour pageData={pageData} setPageData={setPageData}></Tour>
+        </div>
+      ) : (
+        <div className="h-screen flex flex-col items-center">
+          <div className="font-normal text-5xl mt-20">No Tours Left</div>
+          <button
+            className="btn btn-outline btn-primary mt-10"
+            onClick={reloadPage}
+          >
+            Refresh
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Tours;
