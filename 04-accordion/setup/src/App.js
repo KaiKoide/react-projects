30f17eeb647f9dcc56676ef6724766d6a5f4070c
Kai <kkai.chien0813@gmail.com>
2024-02-24 21:20:45 -0800

    feat: implement accordion menu

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <>
+      <div className="bg-cyan-500 flex justify-center items-center">
+        <div className="bg-white w-[90vw] max-w-[800px] h-auto my-16 flex flex-col justify-center items-center p-10">
+          <h2 className="mb-9">questions</h2>
+          <div className="flex flex-col justify-center items-center">
+            {data.map((data) => {
+              return <SingleQuestion {...data} key={data.id} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const handleClick = () => {
+    console.log('clicked!');
+    setIsExpand(!isExpand);
+  };
+
+  return (
+    <div className="shadow-md border-2 w-[45rem] rounded-md mb-6">
+      <div className="p-5 flex justify-between">
+        <h3 className="text-2xl m-0">{title}</h3>
+        <button
+          onClick={handleClick}
+          className="bg-sky-800 hover:bg-sky-700 text-white cursor-pointer w-[2rem] h-[2rem] rounded-full flex items-center justify-center"
+        >
+          {isExpand ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </div>
+      {isExpand ? <p className="mx-5">{info}</p> : ''}
+    </div>
+  );
 };
 
 export default Question;
